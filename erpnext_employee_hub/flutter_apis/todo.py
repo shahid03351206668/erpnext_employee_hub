09870caf4e2412918799471628cbd35e41ed0a11
Malik <shahid03351206668@gmail.com>
2024-12-03 22:31:36 +0500

    Todo fixed

--- erpnext_employee_hub/flutter_apis/todo.py
+++ erpnext_employee_hub/flutter_apis/todo.py
@@ -1,6 +1,12 @@
 import frappe
 from .main import get_user_details
 import json
+import re
+
+
+def striphtml(data):
+    p = re.compile(r"<.*?>")
+    return p.sub("", data)
 
 
 @frappe.whitelist()
@@ -10,10 +16,28 @@ def get_user_todo():
     todos = []
     if user_email:
         todos = frappe.db.sql(
-            f"""SELECT status, description, name, priority FROM `tabToDo`
-        WHERE owner = '{user_email}' """,
+            f"""SELECT 
+            status, 
+            description,
+            name, 
+            priority,
+            allocated_to, 
+            owner AS allocated_by,
+            date,
+            reference_type,
+            reference_name
+            FROM  `tabToDo`
+            WHERE owner = '{user_email}'  OR allocated_to = '{user_email}'
+            ORDER BY date DESC
+            """,
             as_dict=True,
         )
+        for todo in todos:
+            todo["description"] = striphtml(todo.get("description"))
+            # if "description" in todo and todo["description"]:
+            # todo["description"] = html.unescape(
+            #     frappe.utils.strip_html(todo["description"])
+            # )
     frappe.response["data"] = todos
 
 
@@ -30,6 +54,10 @@ def create_todo():
         todo_doc.status = data.get("status")
         todo_doc.description = data.get("description")
         todo_doc.priority = data.get("priority")
+        todo_doc.allocated_to = data.get("allocated_to")
+        todo_doc.date = data.get("date")
+        # todo_doc.reference_type = data.get("reference_type")
+        # todo_doc.reference_name = data.get("reference_name")
 
         todo_doc.flags.ignore_permissions = True
         todo_doc.save()
@@ -53,12 +81,24 @@ def update_todo():
     if data:
         todo_doc = frappe.get_doc("ToDo", data.get("name"))
         if todo_doc:
+
+            # for key in data.keys():
+            #     setattr(todo_doc, key, data[key])
+
             if data.get("status"):
                 todo_doc.status = data.get("status")
             if data.get("description"):
                 todo_doc.description = data.get("description")
             if data.get("priority"):
                 todo_doc.priority = data.get("priority")
+            if data.get("allocated_to"):
+                todo_doc.allocated_to = data.get("allocated_to")
+            if data.get("date"):
+                todo_doc.date = data.get("date")
+            # if data.get("reference_type"):
+            #     todo_doc.allocated_to = data.get("reference_type")
+            # if data.get("reference_name"):
+            #     todo_doc.allocated_to = data.get("reference_name")
 
             todo_doc.flags.ignore_permissions = True
             todo_doc.save()
